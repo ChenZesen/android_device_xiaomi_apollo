c7d38cb580e173ff06ebb4c77cc927ff4add7f43
chenzesen <2535875881@qq.com>
2021-10-29 01:19:00 +0800

    twrp-11

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b twrp-11"
- 
-DT_PATH=device/xiaomi/apollo
-DT_LINK="https://github.com/ChenZesen/android_device_xiaomi_apollo.git -b a11"
 cd /drone/ 
 
 apt install git aria2 rsync curl sudo wget zstd -y
  
-echo "Download the source code"
+#echo "Download the source code"
  
-wget -O - "https://github.com/Mikubill/transfer/releases/download/v0.4.11/transfer_0.4.11_linux_amd64.tar.gz" | tar -x --gzip -C /usr/bin/
+#wget -O - "https://github.com/Mikubill/transfer/releases/download/v0.4.11/transfer_0.4.11_linux_amd64.tar.gz" | tar -x --gzip -C /usr/bin/
 #transfer https://we.tl/t-rR6yeDNa4Y?src=dnl
 #tar axf orangfox.tar.zst
  
@@ -19,6 +14,10 @@ git clone https://gitlab.com/OrangeFox/misc/scripts
 bash scripts/setup/android_build_env.sh
 #bash scripts/setup/install_android_sdk.sh
 #bash scripts/setup/install_android_sdk.sh
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b twrp-11"
+ 
+DT_PATH=device/xiaomi/apollo
+DT_LINK="https://github.com/ChenZesen/android_device_xiaomi_apollo.git -b a11"
 
 echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST
