d1d111811d35fe188dbcaba2e34e40980907d08f
chenzesen <2535875881@qq.com>
2021-10-29 01:30:41 +0800

    twrp-11

--- build.sh
+++ build.sh
@@ -1,25 +1,42 @@
-cd /drone/ 
+#!/bin/bash
 
-apt install git aria2 rsync curl sudo wget zstd -y
- 
-#echo "Download the source code"
- 
-#wget -O - "https://github.com/Mikubill/transfer/releases/download/v0.4.11/transfer_0.4.11_linux_amd64.tar.gz" | tar -x --gzip -C /usr/bin/
-#transfer https://we.tl/t-rR6yeDNa4Y?src=dnl
-#tar axf orangfox.tar.zst
- 
-echo "Setting Up the Compile Environment "
- 
-git clone https://gitlab.com/OrangeFox/misc/scripts
-bash scripts/setup/android_build_env.sh
-#bash scripts/setup/install_android_sdk.sh
-#bash scripts/setup/install_android_sdk.sh
+# Just a basic script U can improvise lateron asper ur need xD 
+
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b twrp-11"
- 
+
 DT_PATH=device/xiaomi/apollo
 DT_LINK="https://github.com/ChenZesen/android_device_xiaomi_apollo.git -b a11"
+
+echo " ===+++ Setting up Build Environment +++==="
+apt install openssh-server -y
+apt update --fix-missing
+apt install openssh-server -y
+mkdir ~/twrp && cd ~/twrp
+DEVICE=${DT_PATH##*\/}
 
 echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST
 repo sync
 git clone --depth=1 $DT_LINK $DT_PATH
+
+echo " ===+++ Building Recovery +++==="
+
+. build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+lunch twrp_${DEVICE}-eng && mka bootimage
+
+#cd $OUT/recovery/root
+#./ldcheck -p system/lib64:vendor/lib64 -d system/bin/qseecomd
+#cd -
+
+# Upload zips & boot.img (U can improvise lateron adding telegram support etc etc)
+echo " ===+++ Uploading Recovery +++==="
+version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+cd out/target/product/$DEVICE
+mv boot.img ${OUTFILE%.zip}.img
+zip -r9 $OUTFILE ${OUTFILE%.zip}.img
+
+#curl -T $OUTFILE https://oshi.at
+curl -sL $OUTFILE https://git.io/file-transfer | sh
+./transfer wet *.zip
